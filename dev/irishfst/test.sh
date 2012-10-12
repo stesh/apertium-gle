97440202602ea990f74c6b8ab71e34be537772ae
stesh <stesh@72bbbca6-d526-0410-a7d9-f06f51895060>
2012-10-12 15:13:07 +0000

    Add morphological guessers from Elaine
    git-svn-id: https://apertium.svn.sourceforge.net/svnroot/apertium/incubator/apertium-gle@41403 72bbbca6-d526-0410-a7d9-f06f51895060

--- dev/irishfst/test.sh
+++ dev/irishfst/test.sh
@@ -4,9 +4,9 @@
 # 2. flookup morphological analysis
 # 3. convert to required CG i/p format
 # 4. vislcg3  disambiguation
-# elaine mar 2012
+# elaine oct 2012
 
-echo "chuaigh siad zzz amach" |  tr -sc "[:alnum:]" "[\n*]" | flookup bin/all.fst | perl dis/lookup2cgg.prl | vislcg3 -g dis/gael-dis.rle 
+cat test.txt |  tr -sc "[:alnum:]" "[\n*]" | flookup -a bin/lexguess.fst | perl dis/lookup2cg3.prl | vislcg3 -g dis/gael-dis.rle 
 
 
 
